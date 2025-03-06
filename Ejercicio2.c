d8196175bb1d4a58831ffc74700aaa21d69269e9
shavima24 <shavi.soto@gmail.com>
2025-03-06 14:24:35 -0600

    Update Ejercicio2.c

--- Ejercicio2.c
+++ Ejercicio2.c
@@ -1,12 +1,20 @@
-//Shari Soto
+//Shari soto
 //Ejercicio 2
 
-if ( a > b)
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
 {
-  printf ("a es mayor \n");
+  int a = 3;
+  int b = 2;
+  if (a > b)
+  {
+    printf("a es mayor \n");
+  } 
+  else
+  {
+      printf("b es mayor o igual a \n");
+  }
+  return 0;
 }
-else
-{
-printf("b es mayor o igual a \n");
-}
-return 0;  
