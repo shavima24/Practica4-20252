89a9a142159bc5eb34ea0084b83b7fd17a093507
shavima24 <shavi.soto@gmail.com>
2025-03-17 18:37:54 -0600

    Ejercicio3.c

--- Ejercicio3.c
+++ Ejercicio3.c
@@ -4,30 +4,30 @@
 int main ()
 {
     int res = 0;
-int i;
+    int i;
 
-for(i = 0; i < 11; i++
+for(i = 0; i < 11; i++)
 {
   res = res + i;
 }
 printf("solucion 1 - El numero es: %d\n", res);
 
 res = 0;
-for(i = 0; i < 11; i++)
+for(i = 1; i <= 10; i++)
 {
   res = res + i;
 }
 printf("solucion 2 - El numero es: %d\n", res);
 
 res = 0;
-for(i = 0; i <= 10; i++
+for(i = 0; i <= 10; i++)
 {
   res = res + i;
 }
 printf("solucion 3 - El numero es: %d\n", res);
 
   res = 0;
-for(i = 1; i < 11; i++)
+for (i = 1; i < 11; i++)
 {
   res = res + i;
 }
